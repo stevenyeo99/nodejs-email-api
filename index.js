3c493627724b9a345770fbbf33e546362d6f0b84
steven.yeo <steven.y@dynrtech.com>
2024-04-13 17:03:25 +0700

    Add CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const nodemailer = require('nodemailer');
+const cors = require('cors');
 
 const PORT = process.env.PORT | 5000;
 const app = express();
